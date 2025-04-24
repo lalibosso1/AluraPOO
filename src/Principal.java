9e6f246a40963eb957fd5429bb8cd2df55c985ec
lalibosso1 <146896466+lalibosso1@users.noreply.github.com>
2025-04-24 16:49:31 -0300

    Atualizando a página principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Principal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "IVE Tour 2024";
+        meuFilme.anoDeLancamento = 2024;
+        meuFilme.duracaoEmMinutos = 180;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println(meuFilme.pegaMedia());
     }
 }
+
+
