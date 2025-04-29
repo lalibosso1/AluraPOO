543467c39d472074c428c735146ce1c069855ca5
lalibosso1 <146896466+lalibosso1@users.noreply.github.com>
2025-04-29 17:15:07 -0300

    refactor: gets and setters

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,21 @@
+import com.lalibosso1.AluraPOO.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "IVE Tour 2024";
-        meuFilme.anoDeLancamento = 2024;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("IVE Tour 2024");
+        meuFilme.setAnoDeLancamento(2024);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoplano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(9);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println("Total de avaliações:" +meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.pegaMedia());
+
+
     }
 }
 
---
+++ src/com/lalibosso1/AluraPOO/modelos/Filme.java
@@ -0,0 +1,63 @@
+package com.lalibosso1.AluraPOO.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoplano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacao;
+    private int duracaoEmMinutos;
+
+    public int getTotalDeAvaliacao(){
+
+        return totalDeAvaliacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoplano() {
+        return incluidoNoplano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void setIncluidoNoplano(boolean incluidoNoplano) {
+        this.incluidoNoplano = incluidoNoplano;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("O nome do filme é : " +nome);
+        System.out.println("O filme lançou em : " +anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacao++;
+
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
+}
+
